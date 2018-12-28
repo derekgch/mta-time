5c8ced993a2bdccca353a76e5070854671d5b745
Derek Chen <derekgc@icloud.com>
2018-12-28 11:44:14 -0500

    trying to fix can't resolve Math/Long error

--- src/adapter.js
+++ src/adapter.js
@@ -2,26 +2,29 @@
 // http://datamine.mta.info/mta_esi.php?key=<key>&feed_id=21
 //
 
-const url =  "http://datamine.mta.info/mta_esi.php?key=";
+const mtaUrl =  "http://datamine.mta.info/mta_esi.php?key=&feed_id=21";
+var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+
 
 export function getFeed(id){
 
-    var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
+    // var GtfsRealtimeBindings = require('gtfs-realtime-bindings');
     var request = require('request');
 
-    var requestSettings = {
-    method: 'GET',
-    url: 'http://datamine.mta.info/mta_esi.php?key=',
-    encoding: null
-    };
-    request(requestSettings, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-        var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
-        feed.entity.forEach(function(entity) {
-        if (entity.trip_update) {
-            // console.log(entity.trip_update);
-        }
-        });
-    }
-    });
+    // var requestSettings = {
+    // method: 'GET',
+    // url: mtaUrl,
+    // encoding: null
+    // };
+    // request(requestSettings, function (error, response, body) {
+    // if (!error && response.statusCode == 200) {
+    //     var feed = GtfsRealtimeBindings.FeedMessage.decode(body);
+    //     console.log(feed);
+    //     // feed.entity.forEach(function(entity) {
+    //     // if (entity.trip_update) {
+    //     //     // console.log(entity.trip_update);
+    //     // }
+    //     // });
+    // }
+    // });
 }
